36b657a461df25eb719baa01cd7e1a0c63a29d29
AniketK2811 <aniketkurve2811@gmail.com>
2023-02-25 15:35:17 +0530

    Service Registry created

---
+++ HomeLoanAppServiceRegistry/src/main/java/com/example/demo/HomeLoanAppServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanAppServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoanAppServiceRegistryApplication.class, args);
+	}
+
+}
